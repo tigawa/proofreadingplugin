ac578d73402c69ad0ede72571fdaa8805037eb39
tigawa <ihciiat@gmail.com>
2013-10-13 22:02:41 +0900

    とりあえず完成

--- background.js
+++ background.js
@@ -3,15 +3,29 @@ chrome.contextMenus.create({
     "type": "normal",
     "contexts": ["selection"],
     "onclick": function(info) {
-        var url = 'http://proofreadingchecker.herokuapp.com/sentence/check?sentence=' + encodeURIComponent(info.selectionText);
-
-//	chrome.tabs.sendRequest(tab.id, param, function(response) {
-//		console.log("highlight response");
-//	});
-		
-		
-        // chrome.tabs.create({
-        //     url: url
-        // });
+     	//var url = 'http://proofreadingchecker.herokuapp.com/sentence/check?sentence=' + encodeURIComponent(info.selectionText);
+     	
+  		//Ajax通信して校正チェックする。
+		$.ajax({
+			type: "GET",
+			url: "http://localhost:3000/sentence/makeup.text",
+			data: "sentence=" + encodeURIComponent(info.selectionText),
+			success: function(msg){
+				//チェック結果をダイアログに表示する。
+				//alert( "Data Saved: " + msg );
+				sendProofreading(msg);
+			}
+		 });
     }
 });
+
+
+/**
+ * メッセージをcontentscriptに送信する。
+ *@parm メッセージ
+ */
+function sendProofreading(msg){
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.sendRequest(tab.id, {message: msg}, function(response) {});
+	});
+}
--- contentscript.js
+++ contentscript.js
@@ -1,23 +1,40 @@
-/**
- * Content script
- * When it receives a request, it Highlights all <a> links in a page.
- */
-chrome.extension.onRequest.addListener (
- //	function (request, sender, sendResponse) {
-		//request.color3
-		
- //		$("*").append('<h1>Hello</h1>');
- 		
- //		$("#s1").dialog({
- // 			autoOpen: false,
- // 			title: 'jQuery Dialog Demo',
- //	 		closeOnEscape: false,
- // 			modal: true,
- // 			buttons: {
- //   				"OK": function(){
- //     					$(this).dialog('close');
- //   				}
- //   			}
- //   		});
- //	}
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    // ダイアログを表示する。
+    showDialog(request.message);
+    // レスポンスを返す。
+    sendResponse({});
+  }
 );
+
+/**
+ *ダイアログを表示する。
+ *@parm message ダイアログに表示するメッセージ
+ */
+function showDialog( message ){
+	var div = document.createElement('div');
+	div.id = 'MSG_DIALOG_20131012152901111';
+	div.innerHTML = message;
+	document.body.appendChild(div);
+
+	$('#MSG_DIALOG_20131012152901111').dialog({
+		autoOpen: true, // ここで起動する。
+		height: "auto",
+		width: "auto",
+		modal: false, // モーダルとして機能しない。
+		closeOnEscape: true, // ESCでダイアログを閉じる
+		closeText: "閉じる", //close 「×」ボタンのツールチップ。
+		buttons: {  	// ダイアログに表示するボタンと処理
+//			"保存": function(){},
+			"閉じる": function(){
+				$('#MSG_DIALOG_20131012152901111').dialog("close");
+			}
+		},	
+		// ダイアログのイベント処理
+		open: function(event, ui) {},
+		close: function() {$('#MSG_DIALOG_20131012152901111').remove();}
+       	 });
+       	 
+       	 //$('#MSG_DIALOG_20131012152901111').dialog("open");
+       	 //$('#MSG_DIALOG_20131012152901111').dialog("moveToTop");
+}
